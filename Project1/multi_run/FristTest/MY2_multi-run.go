5b6edc50b7e65059bb368faf60419d747b2434bb
Magform <nicolas25.02.2003.nf@gmail.com>
2023-03-30 00:45:50 +0200

    Es1V2

---
+++ Project1/multi_run/FristTest/MY2_multi-run.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+const toSearch string = "c"
+const whereSearch string = "aaaaaaaaaaaaabbbbbbbbcccccddddccccccfff"
+
+var mutex sync.Mutex
+var repetitions int = 0
+
+func check(caracter string, wg *sync.WaitGroup) {
+	if caracter == toSearch {
+		mutex.Lock()
+		repetitions++
+		mutex.Unlock()
+	}
+	defer wg.Done()
+}
+
+func code() {
+
+	var wg sync.WaitGroup
+
+	for _, c := range whereSearch {
+		wg.Add(1)
+		go check(string(c), &wg)
+	}
+
+	wg.Wait()
+	fmt.Printf("Il carattere cercato compare %d volte nella stringa.\n", repetitions)
+}
+
+func main() {
+	defer timer("main")() //to see esecution time
+	for i := 0; i < 1000000; i++ {
+		code()
+	}
+}
+
+// To see esecution time
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", name, time.Since(start))
+	}
+}
