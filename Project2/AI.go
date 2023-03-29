93c0c3499be035733e6d5e908e2ca6747b9d5277
Magform <nicolas25.02.2003.nf@gmail.com>
2023-03-29 20:37:15 +0200

    Project2 frist writing

---
+++ Project2/AI.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+// struttura Cliente con il campo "nome"
+type Cliente struct {
+	nome string
+}
+
+// struttura Veicolo con il campo "tipo"
+type Veicolo struct {
+	tipo string
+}
+
+func main() {
+	defer timer("main")() //to see esecution time
+
+	// Inizializzazione del generatore di numeri casuali
+	rand.Seed(time.Now().UnixNano())
+
+	// Definizione dei veicoli disponibili
+	veicoli := []Veicolo{
+		{tipo: "Berlina"},
+		{tipo: "SUV"},
+		{tipo: "Station Wagon"},
+	}
+
+	// Definizione dei clienti
+	clienti := []Cliente{
+		{nome: "Mario"},
+		{nome: "Luigi"},
+		{nome: "Peach"},
+		{nome: "Bowser"},
+		{nome: "Yoshi"},
+		{nome: "Toad"},
+		{nome: "Wario"},
+		{nome: "Waluigi"},
+		{nome: "Donkey Kong"},
+		{nome: "Daisy"},
+	}
+
+	// Array per memorizzare i veicoli noleggiati
+	noleggiati := make(map[string]int)
+
+	// Funzione per noleggiare un veicolo a caso
+	noleggia := func(c Cliente, wg *sync.WaitGroup) {
+		defer wg.Done()
+		v := veicoli[rand.Intn(len(veicoli))]
+		noleggiati[v.tipo]++
+		fmt.Printf("%s ha noleggiato il veicolo %s\n", c.nome, v.tipo)
+	}
+
+	// Sincronizzatore di gruppi di goroutine
+	var wg sync.WaitGroup
+
+	// Noleggio dei veicoli per tutti i clienti
+	for _, c := range clienti {
+		wg.Add(1)
+		go noleggia(c, &wg)
+	}
+
+	// Attende il completamento di tutte le goroutine
+	wg.Wait()
+
+	// Funzione per stampare il numero di Berline, SUV e Station Wagon noleggiati
+	stampa := func() {
+		fmt.Printf("Berline noleggiate: %d\n", noleggiati["Berlina"])
+		fmt.Printf("SUV noleggiate: %d\n", noleggiati["SUV"])
+		fmt.Printf("Station Wagon noleggiate: %d\n", noleggiati["Station Wagon"])
+	}
+
+	// Stampa del numero di veicoli noleggiati
+	stampa()
+}
+
+// To see esecution time
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", name, time.Since(start))
+	}
+}
---
+++ Project2/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+type Cliente struct {
+	nome string
+}
+type Veicolo struct {
+	tipo     string
+	utilized int
+	mutex    sync.Mutex
+}
+
+func noleggia(needRent Cliente, wg *sync.WaitGroup, veicoli *[]Veicolo) {
+	defer wg.Done()
+	var v int = rand.Intn(len(*veicoli))
+
+	(*veicoli)[v].mutex.Lock()
+	(*veicoli)[v].utilized++
+	(*veicoli)[v].mutex.Unlock()
+
+	fmt.Printf("%s ha noleggiato il veicolo %s\n", needRent.nome, (*veicoli)[v].tipo)
+}
+
+func main() {
+	defer timer("main")() //to see esecution time
+	rand.Seed(time.Now().UnixNano())
+
+	veicoliDisponibili := []Veicolo{
+		{tipo: "Berlina",
+			utilized: 0},
+		{tipo: "SUV",
+			utilized: 0},
+		{tipo: "Station Wagon",
+			utilized: 0},
+	}
+
+	clienti := []Cliente{
+		{nome: "Mario"},
+		{nome: "Luigi"},
+		{nome: "Peach"},
+		{nome: "Bowser"},
+		{nome: "Yoshi"},
+		{nome: "Toad"},
+		{nome: "Wario"},
+		{nome: "Waluigi"},
+		{nome: "Donkey Kong"},
+		{nome: "Daisy"},
+	}
+
+	var wg sync.WaitGroup
+
+	for _, c := range clienti {
+		wg.Add(1)
+		go noleggia(c, &wg, &veicoliDisponibili)
+	}
+
+	wg.Wait()
+
+	fmt.Printf("Berline noleggiate: %d\n", veicoliDisponibili[0].utilized)
+	fmt.Printf("SUV noleggiate: %d\n", veicoliDisponibili[1].utilized)
+	fmt.Printf("Station Wagon noleggiate: %d\n", veicoliDisponibili[2].utilized)
+}
+
+// To see esecution time
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", name, time.Since(start))
+	}
+}
