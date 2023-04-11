dedf7ebf627f144e135111b8d2d3897b0d0003d7
Magform <nicolas25.02.2003.nf@gmail.com>
2023-04-11 19:05:58 +0200

    P2 V2 multirun

---
+++ Project2/multi_run/MY2_multi-run.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+	"sync"
+	"sync/atomic"
+	"time"
+)
+
+type Cliente struct {
+	nome string
+}
+type Veicolo struct {
+	tipo     string
+	utilized int32
+}
+
+func noleggia(needRent Cliente, wg *sync.WaitGroup, veicoli *[]Veicolo) {
+	defer wg.Done()
+	var v int = rand.Intn(len(*veicoli))
+
+	atomic.AddInt32(&(*veicoli)[v].utilized, 1)
+
+	fmt.Printf("%s ha noleggiato il veicolo %s\n", needRent.nome, (*veicoli)[v].tipo)
+}
+
+func code() {
+	rand.Seed(time.Now().UnixNano())
+
+	veicoliDisponibili := []Veicolo{
+		{tipo: "Berlina",
+			utilized: 0},
+		{tipo: "SUV",
+			utilized: 0},
+		{tipo: "Station Wagon",
+			utilized: 0},
+	}
+
+	clienti := []Cliente{
+		{nome: "Mario"},
+		{nome: "Luigi"},
+		{nome: "Peach"},
+		{nome: "Bowser"},
+		{nome: "Yoshi"},
+		{nome: "Toad"},
+		{nome: "Wario"},
+		{nome: "Waluigi"},
+		{nome: "Donkey Kong"},
+		{nome: "Daisy"},
+	}
+
+	var wg sync.WaitGroup
+
+	for _, c := range clienti {
+		wg.Add(1)
+		go noleggia(c, &wg, &veicoliDisponibili)
+	}
+
+	wg.Wait()
+
+	fmt.Printf("Berline noleggiate: %d\n", veicoliDisponibili[0].utilized)
+	fmt.Printf("SUV noleggiate: %d\n", veicoliDisponibili[1].utilized)
+	fmt.Printf("Station Wagon noleggiate: %d\n", veicoliDisponibili[2].utilized)
+}
+
+// To see esecution time
+func timer(name string) func() {
+	start := time.Now()
+	return func() {
+		fmt.Printf("%s took %v\n", name, time.Since(start))
+	}
+}
+
+func main() {
+	defer timer("main")() //to see esecution time
+	for i := 0; i < 100000; i++ {
+		code()
+	}
+}
