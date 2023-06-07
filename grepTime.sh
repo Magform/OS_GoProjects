6ff449f47aa7da6411400a6bb8b167f0b9559739
Magform <nicolas25.02.2003.nf@gmail.com>
2023-06-07 10:22:55 +0200

    added P2 to grepTime

--- grepTime.sh
+++ grepTime.sh
@@ -25,3 +25,23 @@ go run ./Project1/multi_run/ThirdTest/MY_multi-run.go | tail -n 1
 go run ./Project1/multi_run/ThirdTest/MY2_multi-run.go | tail -n 1
 go run ./Project1/multi_run/ThirdTest/MY3_multi-run.go | tail -n 1
 go run ./Project1/multi_run/ThirdTest/MY4_multi-run.go | tail -n 1
+
+echo "Project 2"
+
+echo "Test 1"
+
+go run ./Project2/multi_run/FristTest/AI_multi-run.go | tail -n 1
+go run ./Project2/multi_run/FristTest/MY_multi-run.go | tail -n 1
+go run ./Project2/multi_run/FristTest/MY2_multi-run.go | tail -n 1
+
+echo "Test 2"
+
+go run ./Project2/multi_run/SecondTest/AI_multi-run.go | tail -n 1
+go run ./Project2/multi_run/SecondTest/MY_multi-run.go | tail -n 1
+go run ./Project2/multi_run/SecondTest/MY2_multi-run.go | tail -n 1
+
+echo "Test 3"
+
+go run ./Project2/multi_run/ThirdTest/AI_multi-run.go | tail -n 1
+go run ./Project2/multi_run/ThirdTest/MY_multi-run.go | tail -n 1
+go run ./Project2/multi_run/ThirdTest/MY2_multi-run.go | tail -n 1
