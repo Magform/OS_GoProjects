6febdf2a3e579b7f63e0fafb77939a6e293567f0
Magform <nicolas25.02.2003.nf@gmail.com>
2023-06-09 12:08:53 +0200

    dir rename

--- grepTime.sh
+++ grepTime.sh
@@ -4,70 +4,70 @@ echo "Project 1"
 
 echo "Test 1"
 
-go run ./Project1/multi_run/FristTest/AI_multi-run.go | tail -n 1
-go run ./Project1/multi_run/FristTest/MY_multi-run.go | tail -n 1
-go run ./Project1/multi_run/FristTest/MY2_multi-run.go | tail -n 1
-go run ./Project1/multi_run/FristTest/MY3_multi-run.go | tail -n 1
-go run ./Project1/multi_run/FristTest/MY4_multi-run.go | tail -n 1
+go run ./Project1/tests/FristTest/AI_multi-run.go | tail -n 1
+go run ./Project1/tests/FristTest/MY_multi-run.go | tail -n 1
+go run ./Project1/tests/FristTest/MY2_multi-run.go | tail -n 1
+go run ./Project1/tests/FristTest/MY3_multi-run.go | tail -n 1
+go run ./Project1/tests/FristTest/MY4_multi-run.go | tail -n 1
 
 echo "Test 2"
 
-go run ./Project1/multi_run/SecondTest/AI_multi-run.go | tail -n 1
-go run ./Project1/multi_run/SecondTest/MY_multi-run.go | tail -n 1
-go run ./Project1/multi_run/SecondTest/MY2_multi-run.go | tail -n 1
-go run ./Project1/multi_run/SecondTest/MY3_multi-run.go | tail -n 1
-go run ./Project1/multi_run/SecondTest/MY4_multi-run.go | tail -n 1
+go run ./Project1/tests/SecondTest/AI_multi-run.go | tail -n 1
+go run ./Project1/tests/SecondTest/MY_multi-run.go | tail -n 1
+go run ./Project1/tests/SecondTest/MY2_multi-run.go | tail -n 1
+go run ./Project1/tests/SecondTest/MY3_multi-run.go | tail -n 1
+go run ./Project1/tests/SecondTest/MY4_multi-run.go | tail -n 1
 
 echo "Test 3"
 
-go run ./Project1/multi_run/ThirdTest/AI_multi-run.go | tail -n 1
-go run ./Project1/multi_run/ThirdTest/MY_multi-run.go | tail -n 1
-go run ./Project1/multi_run/ThirdTest/MY2_multi-run.go | tail -n 1
-go run ./Project1/multi_run/ThirdTest/MY3_multi-run.go | tail -n 1
-go run ./Project1/multi_run/ThirdTest/MY4_multi-run.go | tail -n 1
+go run ./Project1/tests/ThirdTest/AI_multi-run.go | tail -n 1
+go run ./Project1/tests/ThirdTest/MY_multi-run.go | tail -n 1
+go run ./Project1/tests/ThirdTest/MY2_multi-run.go | tail -n 1
+go run ./Project1/tests/ThirdTest/MY3_multi-run.go | tail -n 1
+go run ./Project1/tests/ThirdTest/MY4_multi-run.go | tail -n 1
 
 echo "Project 2"
 
 echo "Test 1"
 
-go run ./Project2/multi_run/FristTest/AI_multi-run.go | tail -n 1
-go run ./Project2/multi_run/FristTest/MY_multi-run.go | tail -n 1
-go run ./Project2/multi_run/FristTest/MY2_multi-run.go | tail -n 1
+go run ./Project2/tests/FristTest/AI_multi-run.go | tail -n 1
+go run ./Project2/tests/FristTest/MY_multi-run.go | tail -n 1
+go run ./Project2/tests/FristTest/MY2_multi-run.go | tail -n 1
 
 echo "Test 2"
 
-go run ./Project2/multi_run/SecondTest/AI_multi-run.go | tail -n 1
-go run ./Project2/multi_run/SecondTest/MY_multi-run.go | tail -n 1
-go run ./Project2/multi_run/SecondTest/MY2_multi-run.go | tail -n 1
+go run ./Project2/tests/SecondTest/AI_multi-run.go | tail -n 1
+go run ./Project2/tests/SecondTest/MY_multi-run.go | tail -n 1
+go run ./Project2/tests/SecondTest/MY2_multi-run.go | tail -n 1
 
 echo "Test 3"
 
-go run ./Project2/multi_run/ThirdTest/AI_multi-run.go | tail -n 1
-go run ./Project2/multi_run/ThirdTest/MY_multi-run.go | tail -n 1
-go run ./Project2/multi_run/ThirdTest/MY2_multi-run.go | tail -n 1
+go run ./Project2/tests/ThirdTest/AI_multi-run.go | tail -n 1
+go run ./Project2/tests/ThirdTest/MY_multi-run.go | tail -n 1
+go run ./Project2/tests/ThirdTest/MY2_multi-run.go | tail -n 1
 
 echo "Project 3"
 
 echo "Test 1"
 
-go run ./Project3/multi_run/FristTest/AI-HumanFixed_multi-run.go | tail -n 1
-go run ./Project3/multi_run/FristTest/MY_multi-run.go | tail -n 1
-go run ./Project3/multi_run/FristTest/MYV_multi-run.go | tail -n 1
+go run ./Project3/tests/FristTest/AI-HumanFixed_multi-run.go | tail -n 1
+go run ./Project3/tests/FristTest/MY_multi-run.go | tail -n 1
+go run ./Project3/tests/FristTest/MYV_multi-run.go | tail -n 1
 
 echo "Test 1 V2"
 
-go run ./Project3/multi_run/FristTest_V2/AI-HumanFixed_multi-run.go | tail -n 1
-go run ./Project3/multi_run/FristTest_V2/MY_multi-run.go | tail -n 1
-go run ./Project3/multi_run/FristTest_V2/MYV_multi-run.go | tail -n 1
+go run ./Project3/tests/FristTest_V2/AI-HumanFixed_multi-run.go | tail -n 1
+go run ./Project3/tests/FristTest_V2/MY_multi-run.go | tail -n 1
+go run ./Project3/tests/FristTest_V2/MYV_multi-run.go | tail -n 1
 
 echo "Test 2 "
 
-go run ./Project3/multi_run/SecondTest/AI-HumanFixed_multi-run.go | tail -n 1
-go run ./Project3/multi_run/SecondTest/MY_multi-run.go | tail -n 1
-go run ./Project3/multi_run/SecondTest/MYV_multi-run.go | tail -n 1
+go run ./Project3/tests/SecondTest/AI-HumanFixed_multi-run.go | tail -n 1
+go run ./Project3/tests/SecondTest/MY_multi-run.go | tail -n 1
+go run ./Project3/tests/SecondTest/MYV_multi-run.go | tail -n 1
 
 echo "Test 2 V2"
 
-go run ./Project3/multi_run/SecondTest_V2/AI-HumanFixed_multi-run.go | tail -n 1
-go run ./Project3/multi_run/SecondTest_V2/MY_multi-run.go | tail -n 1
-go run ./Project3/multi_run/SecondTest_V2/MYV_multi-run.go | tail -n 1
+go run ./Project3/tests/SecondTest_V2/AI-HumanFixed_multi-run.go | tail -n 1
+go run ./Project3/tests/SecondTest_V2/MY_multi-run.go | tail -n 1
+go run ./Project3/tests/SecondTest_V2/MYV_multi-run.go | tail -n 1
